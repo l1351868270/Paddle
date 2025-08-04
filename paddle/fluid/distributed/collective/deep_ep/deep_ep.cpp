8086a3eb917030fb641ca6176845ec11aada1e74
wuwenkang <wuwenkang@MacBook-Pro.local>
2025-08-04 17:51:07 +0800

    fix illegale memory

--- paddle/fluid/distributed/collective/deep_ep/deep_ep.cpp
+++ paddle/fluid/distributed/collective/deep_ep/deep_ep.cpp
@@ -2317,14 +2317,15 @@ Buffer::m2n_low_latency_dispatch_two_stage(
           phi::DataType::INT32,
           phi::GPUPlace(device_id)));
   auto packed_recv_layout_range = ConvertPaddleTensorToDetailTensor(
-      paddle::experimental::empty({num_local_experts, num_ranks},
+      paddle::experimental::zeros({num_local_experts, num_ranks},
                                   phi::DataType::INT64,
                                   phi::GPUPlace(device_id)));
   auto packed_recv_count =
       ConvertPaddleTensorToDetailTensor(paddle::experimental::empty(
           {num_local_experts}, phi::DataType::INT32, phi::GPUPlace(device_id)));
   auto packed_rdma_recv_count = ConvertPaddleTensorToDetailTensor(
-      paddle::experimental::empty({num_ranks / NUM_MAX_NVL_PEERS},
+      paddle::experimental::full({num_ranks / NUM_MAX_NVL_PEERS},
+                                  -1,
                                   phi::DataType::INT32,
                                   phi::GPUPlace(device_id)));
 
